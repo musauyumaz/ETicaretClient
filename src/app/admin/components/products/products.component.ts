21ec53eccc36b7c4d220d60d1e1d511d246f5dd0
musauyumaznotes <musauyumaznotes@gmail.com>
2022-10-30 13:59:37 +0300

    Ürün Ekleme İşlemleri - ProductService

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -18,46 +17,5 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-
-    this.httpClientService
-      .get<Product[]>({ controller: 'products' })
-      .subscribe((data) => {
-        for (const product of data) {
-          console.log(
-            `İsim : ${product.name} \n Adet:${product.stock}\n Fiyat:${product.price}\n Toplam Tutar:${product.stock * product.price}`
-          );
-        }
-      });
-
-    // this.httpClientService.post(
-    //   {
-    //     controller: 'products',
-    //   },
-    //   {
-    //     name: 'Kalem',
-    //     stock: 100,
-    //     price: 15,
-    //   }
-    // ).subscribe(data=>console.log(data));
-    // this.httpClientService.put({
-    //   controller:"products"
-    // },{
-    //   id:"f3ff36b9-14cd-4d2e-b728-c514d90716cc",
-    //   name:"Karton",
-    //   stock:350,
-    //   price:11
-    // }).subscribe();
-    // this.httpClientService
-    //   .delete(
-    //     { controller: 'products' },
-    //     'f2606182-c9b5-4877-b363-5e830010fcdb'
-    //   )
-    //   .subscribe();
-    // this.httpClientService.get({
-    //   // baseUrl:"https://jsonplaceholder.typicode.com",
-    //   // controller:"posts",
-    //   fullEndPoint:"https://jsonplaceholder.typicode.com/posts"
-    // }).subscribe(data=>console.log(data));
   }
 }
