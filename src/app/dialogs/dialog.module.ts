611f3bb69e1702711670635f3727fc0954618f1b
musauyumaznotes <musauyumaznotes@gmail.com>
2022-11-16 17:15:51 +0300

    Ürünlere Resim Ekleme Operasyonu - 2

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
-import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { SelectProductImageDialogComponent } from './select-product-image-dialog/select-product-image-dialog.component';
 import { FileUploadModule } from '../services/common/file-upload/file-upload.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [DeleteDialogComponent, SelectProductImageDialogComponent],
-  imports: [CommonModule, MatDialogModule, MatButtonModule, FileUploadModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    FileUploadModule,
+    MatCardModule,
+  ],
 })
 export class DialogModule {}
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { List_Product } from 'src/app/contracts/list_product';
+import { List_Product_Image } from 'src/app/contracts/list_product_image';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -39,9 +40,12 @@ export class ProductService {
     size: number = 5,
     successCallBack?: () => void,
     errorCallBack?: (errorMessage: string) => void
-  ): Promise<{totalCount : number,products : List_Product[]}> {
-    const promiseData: Promise<{totalCount : number,products : List_Product[]}> = this.httpClientService
-      .get<{totalCount : number,products : List_Product[]}>({
+  ): Promise<{ totalCount: number; products: List_Product[] }> {
+    const promiseData: Promise<{
+      totalCount: number;
+      products: List_Product[];
+    }> = this.httpClientService
+      .get<{ totalCount: number; products: List_Product[] }>({
         controller: 'products',
         queryString: `page=${page}&size=${size}`,
       })
@@ -55,11 +59,44 @@ export class ProductService {
 
     return await promiseData;
   }
-  async delete(id : string){
-    const deleteObservable: Observable<any> = this.httpClientService.delete<any>({
-      controller:"products"
-    },id);
+  async delete(id: string) {
+    const deleteObservable: Observable<any> =
+      this.httpClientService.delete<any>(
+        {
+          controller: 'products',
+        },
+        id
+      );
 
     await firstValueFrom(deleteObservable);
+  }
+  async readImages(
+    id: string,
+    successCallBack?: () => void
+  ): Promise<List_Product_Image[]> {
+    const getObservable: Observable<List_Product_Image[]> =
+      this.httpClientService.get<List_Product_Image[]>(
+        {
+          action: 'getproductimages',
+          controller: 'products',
+        },
+        id
+      );
+
+    const images: List_Product_Image[] = await firstValueFrom(getObservable);
+    successCallBack();
+    return images;
+  }
+  async deleteImage(id: string, imageId: string, successCallBack?: () => void) {
+    const deleteObservable = this.httpClientService.delete(
+      {
+        action: 'deleteproductimage',
+        controller: 'products',
+        queryString: `imageId=${imageId}`,
+      },
+      id
+    );
+    await firstValueFrom(deleteObservable);
+    successCallBack();
   }
 }
