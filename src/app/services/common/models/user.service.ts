f3aa229e7f98e751362b1105797fe509f50feae1
musauyumaznotes <musauyumaznotes@gmail.com>
2022-11-23 16:10:41 +0300

    Mimarisel Olarak JWT Mantığı ve Konfigürasyonu

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,14 +1,24 @@
+import { Token } from 'src/app/contracts/token/token';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { CreateUser } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private httpClientService: HttpClientService) {}
+  constructor(
+    private httpClientService: HttpClientService,
+    private toastrService: CustomToastrService
+  ) {}
 
   async create(user: User): Promise<CreateUser> {
     const observable: Observable<CreateUser | User> =
@@ -21,15 +31,31 @@ export class UserService {
 
     return (await firstValueFrom(observable)) as CreateUser;
   }
-  async login(usernameOrEmail: string, password: string, callBackFunction? : () => void) : Promise<void> {
-    const observable : Observable<any> = this.httpClientService.post(
+  async login(
+    usernameOrEmail: string,
+    password: string,
+    callBackFunction?: () => void
+  ): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<
+      any | TokenResponse
+    >(
       {
         controller: 'users',
         action: 'login',
       },
       { usernameOrEmail, password }
     );
-      await firstValueFrom(observable);
-      callBackFunction();
+    const tokenResponse: TokenResponse = (await firstValueFrom(observable)) as TokenResponse;
+    if (tokenResponse)
+      localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+      this.toastrService.message(
+        'Kullanıcı girişi başarıyla sağlanmıştır.',
+        'Giriş Başarılı',
+        {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight,
+        }
+      );
+    callBackFunction();
   }
 }
