a976b44224aa0b1e05d8c2a7647c57154bed6fa4
musauyumaznotes <musauyumaznotes@gmail.com>
2022-12-01 14:52:16 +0300

    Mimarisel Olarak SignalR Altyapı Entegrasyonu

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
 import {
   AlertifyOptions,
   AlertifyService,
   MessageType,
   Position,
 } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,12 +17,22 @@ import {
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
-  constructor(private alertify: AlertifyService,spinner : NgxSpinnerService) {
-    super(spinner)
+  constructor(
+    private alertify: AlertifyService,
+    spinner: NgxSpinnerService,
+    private signalRService: SignalRService
+  ) {
+    super(spinner);
+    signalRService.start(HubUrls.ProductHub);
   }
 
   ngOnInit(): void {
-
+    this.signalRService.on(
+      ReceiveFunctions.ProductAddedMessageReceiveFunction,
+      (message) => {
+        this.alertify.message(message,{position : Position.TopRight,messageType: MessageType.Notify});
+      }
+    );
   }
   m() {
     this.alertify.message('Merhaba', {
