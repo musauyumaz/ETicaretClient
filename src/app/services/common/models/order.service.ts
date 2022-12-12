02223c2597a49e9198895b81fcb338bb85c91838
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-12 14:35:43 +0300

    Sipariş Detaylarının Kodlanması

--- src/app/services/common/models/order.service.ts
+++ src/app/services/common/models/order.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Create_Order } from 'src/app/contracts/order/create_order';
 import { List_Order } from 'src/app/contracts/order/list_order';
+import { SingleOrder } from 'src/app/contracts/order/single_order';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -34,6 +35,22 @@ export class OrderService {
     const promiseData = firstValueFrom(observable);
     promiseData
       .then((value) => successCallback())
+      .catch((error) => errorCallBack(error));
+    return await promiseData;
+  }
+  async getOrderById(
+    id: string,
+    successCallback?: () => void,
+    errorCallBack?: (errorMessage: string) => void
+  ) {
+    const observable: Observable<SingleOrder> = this.httpClientService.get<SingleOrder>(
+      {
+        controller: 'orders',
+      },
+      id
+    );
+    const promiseData = firstValueFrom(observable);
+    promiseData.then((value) => successCallback())
       .catch((error) => errorCallBack(error));
     return await promiseData;
   }
