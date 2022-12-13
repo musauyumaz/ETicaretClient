9aaa80e489e10d2de6fb7fc6b654c4d496e263be
Musa UYUMAZ <113431738+musauyumaznotes@users.noreply.github.com>
2022-12-13 18:35:03 +0300

    Reset Password İle Kullanıcı Şifresini Güncelleme

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,26 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
-import { RegisterComponent } from './register/register.component';
 import { RegisterModule } from './register/register.module';
-import { LoginModule } from './login/login.module';
-
-
+import { PasswordResetModule } from './password-reset/password-reset.module';
+import { UpdatePasswordModule } from './update-password/update-password.module';
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ProductsModule,
     BasketsModule,
     HomeModule,
     RegisterModule,
-    // LoginModule
+    // LoginModule,
+    PasswordResetModule,
+    UpdatePasswordModule,
   ],
-  exports:[
-    BasketsModule
-  ]
+  exports: [BasketsModule],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
